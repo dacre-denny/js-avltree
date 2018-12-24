68514b9b8a7c1106c8c36757fd6be988dfc69fb1
Dacre <enquiries@mooce.co.nz>
2018-12-25 00:48:05 +1300

    Added demo script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import { Tree } from "./tree";
+
+
+const tree = new Tree();
+
+for (var i = 0; i < 10000; i++) {
+
+    const node = tree.insert(i);
+
+    console.log("added node:", node.id, "tree height:", tree.root.height);
+}
